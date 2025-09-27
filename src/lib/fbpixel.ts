9131e603631b3102b2c8626c3fb1e6077e30b4e3
Thomas Fabri <tsfabri@gmail.com>
2025-09-27 18:38:23 -0300

    &quote aditions

--- src/lib/fbpixel.ts
+++ src/lib/fbpixel.ts
@@ -3,6 +3,7 @@ export const FB_PIXEL_ID = "1534370980318317";
 // Dispara um evento padrão do Facebook
 export const pageview = () => {
   if (typeof window !== 'undefined' && (window as any).fbq) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     (window as any).fbq("track", "PageView");
   } else {
     console.warn("Facebook Pixel (PageView) not initialized or window.fbq not available.");
@@ -13,6 +14,7 @@ export const pageview = () => {
 export const event = (name: string, options = {}) => {
   if (typeof window !== 'undefined' && (window as any).fbq) {
     console.log(`Facebook Pixel Event: ${name}`, options); // Added console log
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     (window as any).fbq("track", name, options);
   } else {
     console.warn(`Facebook Pixel Event: ${name} not fired. window.fbq not available.`);
